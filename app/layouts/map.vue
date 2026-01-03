35c181118023c30b3eaa8533b03eb47462f69fc6
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-03 19:26:56 +0700

    remove search bar from navbar and adjust wording in sidebar

--- app/layouts/map.vue
+++ app/layouts/map.vue
@@ -17,18 +17,6 @@
                     </label>
                 </div>
                 <div class="flex-1 px-4 font-bold">Peta-Pim</div>
-                <div class="flex-none gap-2">
-                    <label class="input input-sm hidden lg:flex items-center gap-2">
-                        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
-                                stroke="currentColor">
-                                <circle cx="11" cy="11" r="8"></circle>
-                                <path d="m21 21-4.3-4.3"></path>
-                            </g>
-                        </svg>
-                        <input type="search" class="grow w-32 lg:w-48" placeholder="Cari cerita..." />
-                    </label>
-                </div>
             </nav>
         </div>
 
@@ -38,7 +26,7 @@
 
                 <div class="p-4 w-full border-b border-base-300 is-drawer-close:hidden">
                     <h2 class="font-bold text-lg">Peta-Pim</h2>
-                    <p class="text-xs opacity-60">Cerita nusantara</p>
+                    <p class="text-xs opacity-60">Cerita random yang ada di dunia</p>
                 </div>
 
                 <ul class="menu w-full grow">
